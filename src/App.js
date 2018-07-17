90931ca972aaa617a4557f1420e04b0dfde84c7c
Islam <islam.sayed8@gmail.com>
2018-07-17 23:49:54 +0200

    feat: Add changeShelf function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import SearchBooks from './SearchBooks'
 import BookList from './BookList'
@@ -12,9 +12,27 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-
+    books: [],
     showSearchPage: false
   }
+
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+      console.log(books)
+      this.setState({
+        books
+      })
+    })
+  }
+
+  changeShelf = (value, book) => {
+    book.shelf = value
+    console.log(value, book.shelf)
+    this.setState((state) => ({
+      books: state.books.filter((b) => b.id !== book.id).concat([book])
+    }))
+
+}
 
   render() {
     return (
@@ -22,7 +40,7 @@ class BooksApp extends React.Component {
         {this.state.showSearchPage ? (
           <SearchBooks />
           ) : (
-          <BookList />
+          <BookList books={this.state.books} changeShelf={this.changeShelf} />
           )}
       </div>
     )
