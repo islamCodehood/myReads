0bb8bc972ef96f9d3c1ae0013623d78f1e70dfc7
Islam <islam.sayed8@gmail.com>
2018-07-18 00:01:08 +0200

    fix: Add selected attr to move to option

--- src/ShelfChanger.js
+++ src/ShelfChanger.js
@@ -8,9 +8,9 @@ class ShelfChanger extends Component {
         return (
             <div className="book-shelf-changer">
               <select onChange={this.handleChange}>
-                <option value="move" disabled>Move to...</option>
-                <option value="currentlyReading">Currently Reading</option>
+                <option value="move" disabled selected>Move to...</option>
                 <option value="wantToRead">Want to Read</option>
+                <option value="currentlyReading">Currently Reading</option>
                 <option value="read">Read</option>
                 <option value="none">None</option>
               </select>
