c662c2922a71f4ea630ea10d5fc3a9fc6f8f1d1e
Islam <islam.sayed8@gmail.com>
2018-07-17 23:51:47 +0200

    fix: Add props to Book

--- src/Shelf.js
+++ src/Shelf.js
@@ -2,18 +2,25 @@ import React, { Component } from 'react'
 import Book from './Book'
 
 class Shelf extends Component {
+    
     render() {
         return(
             <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
+                  <h2 className="bookshelf-title">{this.props.name}</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
-                      <li>
-                        <Book />
-                      </li>
-                      <li>
-                        <Book />
-                      </li>
+                    {this.props.shelfBooks.filter(book => book.shelf === this.props.value)
+                                          .map(book => (<li key={book.id}>
+                        <Book 
+                            cover={book.imageLinks.thumbnail} 
+                            title={book.title} 
+                            authors={book.authors} 
+                            shelf={book.shelf} 
+                            bookId={book.id}
+                            book={book}
+                            changeShelf={this.props.changeShelf}
+                        />
+                      </li>))}
                     </ol>
                   </div>
                 </div>
