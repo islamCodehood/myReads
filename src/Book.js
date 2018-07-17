b97d40b4b0aeaad433ff71bdea04079cb3f42763
Islam <islam.sayed8@gmail.com>
2018-07-17 23:50:58 +0200

    fix: Add props to shelfChanger

--- src/Book.js
+++ src/Book.js
@@ -2,15 +2,19 @@ import React, { Component } from 'react'
 import ShelfChanger from './ShelfChanger'
 
 class Book extends Component {
+    
     render() {
         return (
             <div className="book">
               <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
-                    <ShelfChanger />
+                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.cover})` }}></div>
+                    <ShelfChanger 
+                        changeShelf={this.props.changeShelf} 
+                        bookId={this.props.bookId}
+                        book={this.props.book} />
                 </div>
-                <div className="book-title">To Kill a Mockingbird</div>
-                <div className="book-authors">Harper Lee</div>
+                <div className="book-title">{this.props.title}</div>
+                <div className="book-authors">{this.props.authors}</div>
             </div>
         )
     }
