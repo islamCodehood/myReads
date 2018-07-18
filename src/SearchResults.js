645eff270f3d8b767e62ccfd2e3329c010ddc8e5
Islam <islam.sayed8@gmail.com>
2018-07-18 16:00:28 +0200

    feat: Create search components

---
+++ src/SearchResults.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class SearchResults extends Component {
+    render() {
+        return(
+            <div className="search-books-results">
+              <ol className="books-grid"></ol>
+            </div>
+        )
+    }
+}
+
+export default SearchResults
