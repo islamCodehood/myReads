d55cf3377e50566d510f0cba6cbf48c8e7c4d955
Islam <islam.sayed8@gmail.com>
2018-07-18 15:58:32 +0200

    fix: Set default value for select

--- src/ShelfChanger.js
+++ src/ShelfChanger.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react'
 
 class ShelfChanger extends Component {
     handleChange = (evt) => {
-        this.props.changeShelf(evt.target.value, this.props.book)
+        this.props.changeShelf(this.props.book, evt.target.value)
     }
+
     render() {
         return (
             <div className="book-shelf-changer">
-              <select onChange={this.handleChange}>
-                <option value="move" disabled selected>Move to...</option>
+                <select
+                    onChange={this.handleChange}
+                    //set the default value to the book current shelf
+                    defaultValue={this.props.currentShelf}
+                >
+                <option value="move" disabled>Move to...</option>
                 <option value="wantToRead">Want to Read</option>
                 <option value="currentlyReading">Currently Reading</option>
                 <option value="read">Read</option>
