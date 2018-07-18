c8a893bf2e70eac523930a7efbb8b315ebddd587
Islam <islam.sayed8@gmail.com>
2018-07-18 15:57:01 +0200

    fix: Add Link component

--- src/AddBook.js
+++ src/AddBook.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class AddBook extends Component {
     render() {
         return (
             <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+              <Link to="/search">Add a book</Link>
             </div>
         )
     }
