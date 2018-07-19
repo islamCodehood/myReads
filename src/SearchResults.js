ac7e3e01536223437f7d9e1877427fb7c15a4415
Islam <islam.sayed8@gmail.com>
2018-07-19 13:04:25 +0200

    feat:Add code for displaying books

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react'
+import Book from './Book'
+import sortBy from 'sort-by'
 
 class SearchResults extends Component {
+  
     render() {
+        let resultedBooks = this.props.books
+        resultedBooks.sort(sortBy('title'))
         return(
             <div className="search-books-results">
-              <ol className="books-grid"></ol>
+                <ol className="books-grid">
+                    {//Check if the search return any result. If so start mapping the array.
+                        resultedBooks.length > 0 && 
+                        //filter only the books that has covers to display.
+                        resultedBooks.filter(book => book.imageLinks !==undefined)
+                                        .map(book => (<li key={book.id}>
+                        <Book 
+                            cover={book.imageLinks.thumbnail} 
+                            title={book.title} 
+                            authors={book.authors} 
+                            bookId={book.id}
+                            book={book}
+                            changeShelf={this.props.changeShelf}
+                            currentShelf={"none"}
+                        />
+                      </li>))}  
+                </ol>
             </div>
         )
     }
